9b49bdadde7de6f3fa7091ea339c9fe0cd803030
luchok503 <lukyannib@mail.ru>
2024-09-30 22:14:03 +0500

    Release #1.0: "Server on Node JS with TODO List Logic"

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
-console.log("Hello, World!");
+import { createServer } from 'node:http';
+import fs from 'node:fs'
+import { changeStatus, createTask, getData, initCounter, deleteTask } from './storage.js';
+
+// const pages = './pages'
+const hostname = '127.0.0.1';
+const port = 3002;
+
+function init() {
+  initCounter()
+}
+const server = createServer((req, res) => {
+  const url = req.url
+  const method = req.method
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+
+  if (url === '/tasks' && method === 'POST') {
+    let body = [];
+    req.on('data', chunk => {
+      body.push(chunk);
+      console.log(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      const item = JSON.parse(body)
+      const result = createTask(item)
+      console.log(result);
+      res.end(result)
+    });
+  }
+
+  // TODO Change Status
+  if (url === '/tasks' && method === 'PATCH') {
+    let body = [];
+    req.on('data', chunk => {
+      body.push(chunk);
+      console.log(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      const requestData = JSON.parse(body)
+      const changedData = changeStatus(requestData.id)
+      res.end(changedData)
+    });
+  }
+
+  // ToDO Delete Task
+  if (url === '/tasks' && method === 'DELETE') {
+    let body = [];
+    req.on('data', chunk => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      const requestData = JSON.parse(body);
+      const result = deleteTask(requestData.id);
+      res.end();
+    });
+  }
+  
+  if (url === '/tasks' && method === 'GET') {
+    const result = getData()
+    res.end(result)
+  }
+
+});
+
+server.listen(port, hostname, () => {
+  init()
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// {
+//   "text": "Hello",
+//   "isDone": "true"
+// }
